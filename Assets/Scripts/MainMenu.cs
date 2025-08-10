680c241b1e6a7a1c048c96c5d376ce8c16d1bbd6
keanenlim123 <s10270417@connect.np.edu.sg>
2025-08-11 05:40:11 +0800

    Added Cutscene
    Cutscene storyline to explain the story monster

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,37 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator cutsceneAnimator;  // Animator for cutscene
+    public GameObject audioObject;     // Audio GameObject to unhide
+    public GameObject buildObject;  
+    
+
     public void PlayGame()
     {
+        Debug.Log("Game started!");
+        StartCoroutine(CutsceneSequence());
+    }
+
+    private IEnumerator CutsceneSequence()
+    {
+        // Play cutscene
+        cutsceneAnimator.SetTrigger("PlayCutscene");
+
+        // Enable audio
+        audioObject.SetActive(true);
+
+        buildObject.SetActive(true);
+
+        // Wait for 33 seconds
+        yield return new WaitForSeconds(33f);
+
+        // Load the next scene in build order
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        Debug.Log("Next scene loaded after 33 seconds.");
     }
     public void QuitGame()
     {
